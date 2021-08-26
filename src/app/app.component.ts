38c36ad29cc76a323c005eef96f2334f83674b8a
graceoswal <graceoswal88@gmail.com>
2021-08-26 16:03:27 +0300

    add class quote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+  quotes:Quote[]=[
+    {id:1, name:'Having a soft heart is not weakness,its courage'},
+    {id:2,name:'On the other side of fear lies freedom'},
+    {id:3,name:'Live life to express not to impress'},
+    {id:4,name:'Work for a cause not applause'},
+    {id:5,name:'Limits like fears are often just an illusion'},
+  
+  ];
 }
