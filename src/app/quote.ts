049fabd620272e86cbc2fa75c9b9721716942942
graceoswal <graceoswal88@gmail.com>
2021-08-26 20:57:33 +0300

    add toggle details

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,9 +1,13 @@
 export class Quote {
+ showDescription: boolean;
 
   constructor (
     public id:number,
     public name: string, 
     public quote:string,
      public author: string,
-     public showInfo: string){}
+     public description : string,
+     ){
+       this.showDescription=false;
+     }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,17 +8,22 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes:Quote[]=[
-    {id:1, name:'Wendy',quote:'Having a soft heart is not weakness,its courage',author: 'Grace', showInfo:'Courage'},
-    {id:2,name:'Richard',quote:'On the other side of fear lies freedom',author: 'Nelson',showInfo:'Freedom'},
-    {id:3,name:'Suzan',quote:'Live life to express not to impress',author: 'Meghan M' ,showInfo:'Self-discipline'},
-    {id:4,name:'Maureen',quote:'Work for a cause not applause', author: 'Limumba P',showInfo:'Determination'},
-    {id:5,name:'Kimathi',quote:'Limits like fears are often just an illusion',author:'Michael J', showInfo: 'Brave'},
-  
+    new Quote(1, 'Wendy', 'Having a soft heart is not weakness,its courage', 'Grace', 'Courage'),
+    new Quote(2,'Richard','On the other side of fear lies freedom', 'Nelson','Freedom'),
+    new Quote(3,'Suzan','Live life to express not to impress','Meghan M' ,'Self-discipline'),
+    new Quote(4,'Maureen','Work for a cause not applause', 'Limumba P','Determination'),
+    new Quote(5,'Kimathi','Limits like fears are often just an illusion','Michael J',  'Brave'),
   ];
+toggleDetails(index: any){
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
+
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
+
+
